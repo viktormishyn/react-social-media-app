31f814ccf7cc64f5862ca723ab61fd2dff4c70fc
Viktor Mishyn <viktormishyn@gmail.com>
2021-01-05 12:02:39 +0200

    28. Move posts data from MyPosts.jsx to Profile.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,17 +1,9 @@
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
-const MyPosts = () => {
+const MyPosts = (props) => {
 
-    let posts = [
-        {id: 1, post: 'Hi, how are you?', likesCount: 12},
-        {id: 2, post: 'Boring post about something boring', likesCount: 3},
-        {id: 2, post: 'A lot of work today', likesCount: 7},
-        {id: 2, post: 'My first post', likesCount: 11},
-
-    ]
-
-    let postsElements = posts.map(p => (<Post message={p.post}/>))
+    let postsElements = props.posts.map(p => (<Post message={p.post}/>))
 
     return (
         <div className={s.postsBlock}>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,10 +3,19 @@ import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
+
+    let posts = [
+        {id: 1, post: 'Hi, how are you?', likesCount: 12},
+        {id: 2, post: 'Boring post about something boring', likesCount: 3},
+        {id: 2, post: 'A lot of work today', likesCount: 7},
+        {id: 2, post: 'My first post', likesCount: 11},
+
+    ]
+
     return (
         <div>
             <ProfileInfo/>
-            <MyPosts/>
+            <MyPosts posts={posts}/>
         </div>
     );
 }
