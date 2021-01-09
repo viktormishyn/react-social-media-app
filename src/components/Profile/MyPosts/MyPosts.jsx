5b49c2f24d491a0e90ea01d7a55c81902f8b3e27
Viktor Mishyn <viktormishyn@gmail.com>
2021-01-09 21:53:20 +0200

    31. Create ref to the element in MyPosts.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,19 +1,29 @@
 import s from './MyPosts.module.css'
-import Post from "./Post/Post";
+import React from 'react'
+import Post from "./Post/Post"
+
+
 
 const MyPosts = (props) => {
 
     let postsElements = props.posts.map(p => (<Post message={p.post}/>))
+
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text)
+    }
 
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
 
