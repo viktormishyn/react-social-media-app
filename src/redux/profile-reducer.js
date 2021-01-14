0a984f4320d784d0fe7f8e95a574baf2d7fa6bfa
Viktor Mishyn <viktormishyn@gmail.com>
2021-01-14 20:57:32 +0200

    41. Refactor if-else if constructions in reducers into switch cases

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -2,18 +2,22 @@ const ADD_POST = "ADD-POST"
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT"
 
 const profileReducer = (state, action) => {
-    if (action.type === ADD_POST) {
-        let newPost = {
-            id: 5,
-            post: state.newPostText,
-            likesCount: 0
-        }
-        state.posts.push(newPost)
-        state.newPostText = ''
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-        state.newPostText = action.newText
+    switch (action.type) {
+        case ADD_POST:
+            let newPost = {
+                id: 5,
+                post: state.newPostText,
+                likesCount: 0
+            }
+            state.posts.push(newPost)
+            state.newPostText = ''
+            return state
+        case UPDATE_NEW_POST_TEXT:
+            state.newPostText = action.newText
+            return state
+        default:
+            return state
     }
-    return state;
 }
 
 export default profileReducer;
