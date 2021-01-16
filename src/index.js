21afe9e24b5e0ec139aa587c2dfdc346e95b1f0c
Viktor Mishyn <viktormishyn@gmail.com>
2021-01-16 11:49:11 +0200

    42. Subscribe anonymous function which calls rerenderEntireTree in order to pass store.getState() as attribute

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
-            <App state={store.getState()}
+            <App state={state}
                  dispatch={store.dispatch.bind(store)}/>
         </React.StrictMode>,
         document.getElementById('root')
@@ -16,4 +16,6 @@ let rerenderEntireTree = (state) => {
 
 rerenderEntireTree(store.getState())
 
-store.subscribe(rerenderEntireTree)
+store.subscribe(() => {
+    rerenderEntireTree(store.getState())
+})
