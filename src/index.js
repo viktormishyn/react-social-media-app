de6d053a87760251a9da3a526eb82d9f3190d88d
Viktor Mishyn <viktormishyn@gmail.com>
2021-01-18 17:52:39 +0200

    44. Add <Provider> to StoreContext.js and wrap App component with Provider in index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import StoreContext from "./StoreContext";
+import {Provider} from "./StoreContext";
 
 let rerenderEntireTree = () => {
     ReactDOM.render(
         <React.StrictMode>
-            <StoreContext.Provider value={store}>
-                <App store={store}/>
-            </StoreContext.Provider>
+            <Provider store={store}>
+                <App/>
+            </Provider>
         </React.StrictMode>,
         document.getElementById('root')
     );
