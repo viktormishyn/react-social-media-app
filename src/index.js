ad296d7187f061deea924f214de2bcfb14a14de1
Viktor Mishyn <viktormishyn@gmail.com>
2021-01-19 16:26:54 +0200

    47. Delete rerenderEntireTree() and Subscribe from index.js

--- src/index.js
+++ src/index.js
@@ -5,17 +5,11 @@ import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
 
-let rerenderEntireTree = () => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
-}
 
-rerenderEntireTree(store.getState())
-
-store.subscribe(rerenderEntireTree)
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root'))
