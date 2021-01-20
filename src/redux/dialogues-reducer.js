3891672c0afda66143d4ccc8c37c1e6d95fa5cda
Viktor Mishyn <viktormishyn@gmail.com>
2021-01-20 15:00:22 +0200

    48. Add deep copies of state parts in dialogues-reducer.js. Refactor

--- src/redux/dialogues-reducer.js
+++ src/redux/dialogues-reducer.js
@@ -24,13 +24,17 @@ let initialState = {
 const dialoguesReducer = (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
-            state.newMessageBody = action.body
-            return state
+            return {
+                ...state,
+                newMessageBody: action.body
+            }
         case SEND_MESSAGE:
             let body = state.newMessageBody
-            state.messages.push({id: 6, message: body})
-            state.newMessageBody = ''
-            return state
+            return {
+                ...state,
+                newMessageBody: '',
+                messages: [...state.messages, {id: 6, message: body}]
+            }
         default:
             return state
     }
