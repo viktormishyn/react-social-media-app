fc0fdaf76fe37a5fd2f94680dbe80579fe42afe7
Viktor Mishyn <viktormishyn@gmail.com>
2021-01-21 19:44:45 +0200

    49. Fix bug with users being added to state twice - delete <React.StrictMode> from index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import {Provider} from "react-redux";
 
 
 ReactDOM.render(
-    <React.StrictMode>
         <Provider store={store}>
             <App/>
-        </Provider>
-    </React.StrictMode>,
+        </Provider>,
     document.getElementById('root'))
