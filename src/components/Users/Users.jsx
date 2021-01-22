8526b79c87c1b5cc396c295c3be8212a7a21265d
Viktor Mishyn <viktormishyn@gmail.com>
2021-01-22 22:28:23 +0200

    50. Modify Users.jsx according to API data. Add temporary generic avatar for all users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,10 +1,11 @@
 import s from './Users.module.css'
 import * as axios from 'axios'
+import userPhoto from '../../assets/images/generic_avatar.png'
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(respose => {
-            props.setUsers(respose.data.items)
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items)
         })
 
     }
@@ -14,7 +15,7 @@ let Users = (props) => {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={s.userPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto}/>
                     </div>
                     <div>
                             {u.followed
@@ -28,12 +29,12 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
