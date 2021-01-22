44d0aaeea7cf414b9139ef9d66b1dac7c48da41c
Viktor Mishyn <viktormishyn@gmail.com>
2021-01-22 22:12:15 +0200

    50. Get users data through axios request instead of hard-coded data in Users.jsx

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,43 +1,12 @@
 import s from './Users.module.css'
+import * as axios from 'axios'
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers(
-            [
-                {
-                    id: 1,
-                    photoUrl: "https://www.shareicon.net/data/256x256/2016/06/25/786530_people_512x512.png",
-                    followed: false,
-                    fullName: "Ben",
-                    status: "make it easy!",
-                    location: {city: "London", country: "United Kingdom"}
-                },
-                {
-                    id: 2,
-                    photoUrl: "https://at-cdn-s02.audiotool.com/2020-05-27/users/drugs/avatar256x256-4ed4d644600947459e98332efbc552e7.jpg",
-                    followed: true,
-                    fullName: "Ivan",
-                    status: "I like nice girls",
-                    location: {city: "Moscow", country: "Russia"}
-                },
-                {
-                    id: 3,
-                    photoUrl: "https://i.pinimg.com/474x/3b/2a/e4/3b2ae4b99fcd5be3398009872c025263.jpg",
-                    followed: true,
-                    fullName: "Mary",
-                    status: "I like bad boys=)",
-                    location: {city: "Kiev", country: "Ukraine"}
-                },
-                {
-                    id: 4,
-                    photoUrl: "https://cdn.iconscout.com/icon/premium/png-256-thumb/female-student-avatar-1837115-1559163.png",
-                    followed: true,
-                    fullName: "Diana",
-                    status: "trees are funny",
-                    location: {city: "London", country: "United Kingdom"}
-                }
-            ]
-        )
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(respose => {
+            props.setUsers(respose.data.items)
+        })
+
     }
 
     return <div>
