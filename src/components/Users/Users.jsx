1c303889062a0479dbaefe047c15c9110e1106ac
Viktor Mishyn <viktormishyn@gmail.com>
2021-01-23 23:20:30 +0200

    52. Transform server request into onClick callback in Users.jsx

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,14 +3,18 @@ import * as axios from 'axios'
 import userPhoto from '../../assets/images/generic_avatar.png'
 
 let Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items)
-        })
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items)
+            })
 
+        }
     }
 
+
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
