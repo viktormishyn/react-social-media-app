a05acd11a6d836eb666bbe0efa67c1f6f63c58f9
Viktor Mishyn <viktormishyn@gmail.com>
2021-02-01 23:41:57 +0200

    55. Create users pagination counter

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,14 +6,28 @@ import React from "react"
 class Users extends React.Component {
 
     componentDidMount() {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
             this.props.setUsers(response.data.items)
         })
     }
 
     render() {
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
+
+        let pages = []
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i)
+        }
+
         return (
             <div>
+                <div>
+                    {pages.map(p => {
+                        return <span className={this.props.currentPage === p && s.selectedPage}>{p}</span>
+                        // only if true {true && s.selectedPage}. Another way {true ? s.selectedPage : ""}
+                    })}
+                </div>
                 {
                     this.props.users.map(u => <div key={u.id}>
                 <span>
