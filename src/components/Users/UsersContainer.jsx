0711c5198d68d17d198403bd292a3d8432979bde
Viktor Mishyn <viktormishyn@gmail.com>
2021-02-06 23:59:33 +0200

    Create component Preloader in src/components/common/

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -11,6 +11,7 @@ import {
 } from "../../redux/users-reducer";
 import * as axios from "axios";
 import preloader from "../../assets/images/preloader.svg"
+import Preloader from "../common/Preloader/Preloader";
 
 class UsersContainer extends React.Component {
 
@@ -34,7 +35,7 @@ class UsersContainer extends React.Component {
 
     render() {
         return <>
-            {this.props.isFetching ? <img src={preloader}/> : null}
+            {this.props.isFetching ? <Preloader/> : null}
             <Users totalUsersCount={this.props.totalUsersCount}
                    pageSize={this.props.pageSize}
                    currentPage={this.props.currentPage}
