0dc3facc4f2b9e6d3d2fe7a7877e34d99ec12085
Viktor Mishyn <viktormishyn@gmail.com>
2021-02-08 16:02:33 +0200

    59. Create ProfileContainer class component in ProfileContainer.jsx

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import {BrowserRouter, Route} from "react-router-dom";
 
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import DialoguesContainer from "./components/Dialogues/DialoguesContainer";
 import UsersContainer from "./components/Users/UsersContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
 
 const App = (props) => {
 
@@ -21,7 +21,7 @@ const App = (props) => {
                     <Route path="/dialogues"
                            render={() => <DialoguesContainer/>}/>
                     <Route path="/profile"
-                           render={() => <Profile/>}/>
+                           render={() => <ProfileContainer/>}/>
                     <Route path="/users"
                            render={() => <UsersContainer/>}/>
 
