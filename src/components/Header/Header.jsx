e825e639663395c8630c58499725d92f8aa7ef09
Viktor Mishyn <viktormishyn@gmail.com>
2021-02-16 16:45:13 +0200

    61. Add 'Login' Navlink to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import s from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
     return (
         <header className={s.header}>
             <img src="https://www.humanrightslogo.net/sites/default/files/HRLogoCMYK.svg" alt="img from internet"/>
+            <div className={s.loginBlock}>
+                <NavLink to={'/login'}>Login</NavLink>
+            </div>
         </header>
     );
 }
