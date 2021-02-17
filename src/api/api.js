1fb8a07a62ce47da828de549ee16891d20d566eb
Viktor Mishyn <viktormishyn@gmail.com>
2021-02-17 12:14:22 +0200

    63. Refactor (create baseUrl const) in api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,9 @@
 import * as axios from "axios";
 
+const baseUrl = 'https://social-network.samuraijs.com/api/1.0/'
 
 export const getUsers = (currentPage, pageSize) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
+    return axios.get(baseUrl + `users?page=${currentPage}&count=${pageSize}`,
         {
             withCredentials: true
         })
