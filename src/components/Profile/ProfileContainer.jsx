70fed34fd95134901e7eca743aeaad1a6b633e88
Viktor Mishyn <viktormishyn@gmail.com>
2021-02-20 20:56:02 +0200

    67. Create getUserProfile thunk-creator in profile-reducer.js. Refactor ProfileContainer.jsx

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,10 +1,8 @@
 import React from "react"
 import Profile from "./Profile";
-import * as axios from "axios";
-import {setUserProfile} from "../../redux/profile-reducer";
+import {getUserProfile} from "../../redux/profile-reducer";
 import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
-import {usersAPI} from "../../api/api";
 
 class ProfileContainer extends React.Component {
 
@@ -13,9 +11,7 @@ class ProfileContainer extends React.Component {
         if (!userId) {
             userId = 2
         }
-        usersAPI.getProfile(userId).then(response => {
-            this.props.setUserProfile(response.data)
-        })
+        this.props.getUserProfile(userId)
     }
 
     render() {
@@ -33,4 +29,4 @@ let mapStateToProps = (state) => ({
 
 let WithUrlDataContainerComponent = withRouter(ProfileContainer)
 
-export default connect(mapStateToProps, {setUserProfile})(WithUrlDataContainerComponent)
+export default connect(mapStateToProps, {getUserProfile})(WithUrlDataContainerComponent)
