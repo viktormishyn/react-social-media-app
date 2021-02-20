ff9424722456a8451fad4042c99cefd2ca51d52b
Viktor Mishyn <viktormishyn@gmail.com>
2021-02-20 14:03:31 +0200

    66. Create follow and unfollow methods for usersAPI in api.js

--- src/api/api.js
+++ src/api/api.js
@@ -12,6 +12,12 @@ export const usersAPI = {
     getUsers(currentPage, pageSize) {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`)
             .then(response => response.data)
+    },
+    follow(userId) {
+        return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+    },
+    unfollow(userId) {
+        return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
     }
 }
 
