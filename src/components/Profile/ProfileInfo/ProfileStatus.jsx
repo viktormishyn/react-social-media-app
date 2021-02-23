442d3413334ad8a4c73bbcac314056270e867e7d
Viktor Mishyn <viktormishyn@gmail.com>
2021-02-23 19:37:56 +0200

    71. Add activateEditMode and deactivateEditMode methods in ProfileStatus

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -6,16 +6,30 @@ class ProfileStatus extends React.Component {
         editMode: false
     }
 
+    activateEditMode = () => {
+        this.setState({
+            editMode: true
+        })
+        // this.state.editMode = true
+        // this.forceUpdate()
+    }
+
+    deactivateEditMode = () => {
+        this.setState({
+            editMode: false
+        })
+    }
+
     render() {
         return (
             <div>
                 {!this.state.editMode &&
                 <div>
-                    <span>{this.props.status}</span>
+                    <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
                 </div>}
                 {this.state.editMode &&
                 <div>
-                    <input value={this.props.status}/>
+                    <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/>
                 </div>}
             </div>
         );
