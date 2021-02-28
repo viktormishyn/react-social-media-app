1e8ede7dc6d2d99878f531b161deb335c7639ef5
Viktor Mishyn <viktormishyn@gmail.com>
2021-02-28 11:34:12 +0200

    73. In api.js create profileAPI with getStatus and updateStatus methods. Update profile-reducer.js and Profile components

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Profile from "./Profile";
-import {getUserProfile} from "../../redux/profile-reducer";
+import {getUserProfile, getStatus, updateStatus} from "../../redux/profile-reducer";
 import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
@@ -14,23 +14,29 @@ class ProfileContainer extends React.Component {
             userId = 2
         }
         this.props.getUserProfile(userId)
+        this.props.getStatus(userId)
+        // this.props.updateStatus(status)
     }
 
     render() {
         return (
             <div>
-                <Profile {...this.props} profile={this.props.profile}/>
+                <Profile {...this.props}
+                         profile={this.props.profile}
+                         status={this.props.status}
+                         updateStatus={this.props.updateStatus}/>
             </div>
         )
     }
 }
 
 let mapStateToProps = (state) => ({
-    profile: state.profilePage.profile
+    profile: state.profilePage.profile,
+    status: state.profilePage.status
 })
 
 export default compose(
-    connect(mapStateToProps, {getUserProfile}),
+    connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
     withRouter,
     withAuthRedirect)
 (ProfileContainer)
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -15,7 +15,7 @@ const ProfileInfo = (props) => {
             {/*</div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
-                <ProfileStatus status={"Hello world"}/>
+                <ProfileStatus status={props.status}/>
             </div>
         </div>
     );
